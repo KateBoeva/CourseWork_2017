0f288b49781e2179e34943386efb18c0f8fa1ee6
katemrrr <katrinb_08@mail.ru>
2017-12-17 16:17:43 +0300

    add components and styles

---
+++ src/app/hero.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by katemrrr on 17.12.17.
+ */
+
+export class Hero {
+    id: number;
+    name: string;
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Mom'},
+    { id: 10, name: 'Kate'},
+    { id: 11, name: 'Dasha'},
+    { id: 12, name: 'Ann'},
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
